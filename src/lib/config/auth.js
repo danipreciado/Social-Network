7205bad3367cfa1a76df48b0ac04f8941edc2117
Angy Rincón <rinconmezangelica@gmail.com>
2023-04-25 01:51:04 -0600

    pruebas de test

--- src/lib/config/auth.js
+++ src/lib/config/auth.js
@@ -1,39 +1,40 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from './firebaseconfig';
 
-function errorMessages(errorCode, errorEmailMessage, errorPassMessage) {
-  if (errorCode === 'auth/email-already-in-use') {
-    errorEmailMessage.innerHTML = 'Este correo ya ha sido registrado';
-  } else if (errorCode === 'auth/weak-password') {
-    errorPassMessage.innerHTML = 'Escribe una contraseña más larga';
-  } else if (errorCode === 'auth/invalid-email') {
-    errorEmailMessage.innerHTML = 'Escribe un correo valido';
-  } else if (errorCode === 'auth/missing-password') {
-    errorPassMessage.innerHTML = 'Escribe una contraseña valida';
-  }
-}
+// function errorMessages(errorCode, errorEmailMessage, errorPassMessage) {
+//   if (errorCode === 'auth/email-already-in-use') {
+//     errorEmailMessage.innerHTML = 'Este correo ya ha sido registrado';
+//   } else if (errorCode === 'auth/weak-password') {
+//     errorPassMessage.innerHTML = 'Escribe una contraseña más larga';
+//   } else if (errorCode === 'auth/invalid-email') {
+//     errorEmailMessage.innerHTML = 'Escribe un correo valido';
+//   } else if (errorCode === 'auth/missing-password') {
+//     errorPassMessage.innerHTML = 'Escribe una contraseña valida';
+//   }
+// }
 
 export function authFunction(signup, userEmail, userPassword, onAuthSuccess) {
-  const errorEmailMessage = document.querySelector('#errorEmailMessage');
-  const errorPassMessage = document.querySelector('#errorPassMessage');
+  // const errorEmailMessage = document.querySelector('#errorEmailMessage');
+  // const errorPassMessage = document.querySelector('#errorPassMessage');
   signup.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const email = userEmail.value;
     const password = userPassword.value;
-    errorEmailMessage.innerHTML = '';
-    errorPassMessage.innerHTML = '';
+    // errorEmailMessage.innerHTML = '';
+    // errorPassMessage.innerHTML = '';
 
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
+        // eslint-disable-next-line no-unused-vars
         const user = userCredential.user;
         onAuthSuccess('/verification');
       })
       .catch((error) => {
         const errorCode = error.code;
         console.log(errorCode);
-        errorMessages(errorCode, errorEmailMessage, errorPassMessage);
+        // errorMessages(errorCode, errorEmailMessage, errorPassMessage);
       });
   });
 }
---
+++ test/index.spec.js
@@ -0,0 +1,43 @@
+import { authFunction2 } from './mocks';
+
+jest.mock('../src/lib/config/auth');
+
+// Definir los valores para el email y la contraseña
+// const email = 'test@example.com';
+// const password = 'test123';
+
+// Crear el test usando Jest
+test('authFunction2 resolves with a user object with valid parameters', async () => {
+  expect.assertions(1);
+  try {
+    const user = await authFunction2({ value: 'test@test.com' }, { value: 'password123' });
+    expect(user).toBeDefined();
+  } catch (error) {
+    throw new Error('The authFunction2 function should not reject with valid parameters');
+  }
+});
+test('createUserWithEmailAndPassword throws an error with invalid email and password', async () => {
+  expect.assertions(1);
+  try {
+    // Llamar a la función authFunction2 con un email y una contraseña inválidos
+    await authFunction2('invalidEmail', 'invalidPassword');
+
+    // Si la función no devuelve ningún error, lanzar una excepción
+    // eslint-disable-next-line max-len
+    // throw new Error('The createUserWithEmailAndPassword function should throw an error with invalid email and password');
+  } catch (error) {
+    // Comprobar que el mensaje de error es el esperado
+    expect(error.message).toEqual('Firebase: Error (auth/admin-restricted-operation).');
+  }
+});
+
+// const authFunction = (email, password) => new Promise((resolve, reject) => {
+//   if (!email || !password) {
+//     PromiseRejectionEvent('error');
+//   }
+//   resolve('Bien!');
+// });
+
+// test('authFunction', async () => {
+//   await (createaccount('email', '')).resolves.toBe('error');
+// });
