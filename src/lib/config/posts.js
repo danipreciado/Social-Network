6719074b38df39ff580ac25118b0068afdfd2b4e
danipreciado <danielapca96@gmail.com>
2023-05-08 06:40:03 -0700

    eliminar post y boton de google siguiendo lineaminetos

--- src/lib/config/posts.js
+++ src/lib/config/posts.js
@@ -1,11 +1,13 @@
-import { addDoc, getDocs, serverTimestamp } from 'firebase/firestore';
+import {
+  addDoc, getDocs, serverTimestamp, deleteDoc, doc,
+} from 'firebase/firestore';
 import { colRef, auth } from './firebaseconfig.js';
 
 getDocs(colRef)
   .then((snapshot) => {
     const posts = [];
-    snapshot.docs.forEach((doc) => {
-      posts.push({ ...doc.data(), id: doc.id });
+    snapshot.docs.forEach((docu) => {
+      posts.push({ ...docu.data(), id: docu.id });
     });
     console.log(posts);
   })
@@ -24,4 +26,16 @@ export function posting(input, form) {
       form.reset();
     });
   console.log(getDocs(colRef));
+}
+
+export function deletePost(postId) {
+  const postDocRef = doc(colRef, postId);
+
+  return deleteDoc(postDocRef)
+    .then(() => {
+      console.log(`Post ${postId} deleted successfully`);
+    })
+    .catch((err) => {
+      console.log(`Error deleting post ${postId}: ${err.message}`);
+    });
 }
