78f8168e094c8bb36856e5d9d728e9e3e9c1d52c
Angy Rincón <rinconmezangelica@gmail.com>
2023-05-12 15:41:57 -0600

    test coverage 308-325

--- test/wall.spec.js
+++ test/wall.spec.js
@@ -1,14 +1,15 @@
 /**
  * @jest-environment jsdom
  */
+import { getAuth } from 'firebase/auth';
 import { wall } from '../src/templates/wall';
 import { signOutUser } from '../src/lib/config/auth';
-import { posting, postData } from '../src/lib/config/posts';
+import { posting, postData, deletePost } from '../src/lib/config/posts';
 
 jest.mock('firebase/auth', () => ({
   getAuth: () => ({
     currentUser: {
-      displayName: 'John Doe',
+      displayName: 'user1',
     },
   }),
 }));
@@ -20,7 +21,7 @@ jest.mock('../src/lib/config/auth', () => ({
 jest.mock('../src/lib/config/posts', () => ({
   posting: jest.fn(),
   postData: jest.fn(),
-
+  deletePost: jest.fn(),
 }));
 
 describe('wall', () => {
@@ -67,6 +68,17 @@ describe('wall', () => {
 
     expect(posting).toHaveBeenCalledWith(input, form);
   });
+  test('Si el input está vacío np debe llamarse la función posting', () => {
+    const container = document.createElement('section');
+    container.append(wall());
+    const form = container.querySelector('.form-yourpost');
+    const input = container.querySelector('.your-postInput');
+    input.value = '';
+
+    form.dispatchEvent(new Event('submit', { bubbles: true }));
+
+    expect(posting).not.toHaveBeenCalledWith(input, form);
+  });
 
   test('al hacer clic en el menú hamburguesa la clase debe cambiar a active', () => {
     const container = document.createElement('section');
@@ -96,10 +108,50 @@ describe('wall', () => {
         callback(mockPos);
       }),
     };
-
+    postData.mockImplementationOnce((callback) => {
+      callback(mockQuerySnapshot);
+    });
+    const container = wall();
+    const auth = getAuth();
+    mockQuerySnapshot.forEach((mockPos) => {
+      const deleteButton = container.querySelector('.option2');
+      const confirmationModal = container.querySelector('.confirmationModal');
+      const confirmBtn = container.querySelector('.confirmBtn');
+      const cancelBtn = container.querySelector('.cancelBtn');
+      deleteButton.click();
+      expect(confirmationModal.style.display).toBe('block');
+      confirmBtn.click();
+      expect(deletePost).toHaveBeenCalledWith(mockPos.id);
+      expect(confirmationModal.style.display).toBe('none');
+      // 318
+      cancelBtn.click();
+      expect(confirmationModal.style.display).toBe('none');
+      expect(mockPos.data().userid).toEqual(auth.currentUser.displayName);
+    });
+  });
+  it('username del currentuser no es igual al del post', () => {
+    const mockQuerySnapshot = {
+      forEach: jest.fn((callback) => {
+        const mockPostData = {
+          userid: 'user2',
+          text: 'Some post text',
+          likes: [],
+          likescat: [],
+        };
+        const mockPos = {
+          id: 'post1',
+          data: () => mockPostData,
+        };
+        callback(mockPos);
+      }),
+    };
     postData.mockImplementationOnce((callback) => {
       callback(mockQuerySnapshot);
     });
     wall();
+    const auth = getAuth();
+    mockQuerySnapshot.forEach((mockPos) => {
+      expect(mockPos.data().userid).not.toEqual(auth.currentUser.displayName);
+    });
   });
 });
